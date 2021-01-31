d259bc57aec5123fba8232faa2c396d6fcab183b
lasha tatulashvili <lashatatu@gmail.com>
2021-01-31 22:35:26 +0400

    api problem

--- pages/api/v1/portfolios.js
+++ pages/api/v1/portfolios.js
@@ -4,7 +4,7 @@ import auth0 from '@/utils/auth0';
 export default async function createPortfolio(req, res) {
   try {
     const { accessToken } = await auth0.getSession(req);
-    const createdPortfolio=await new PortfolioApi(accessToken).createPortfolio(req.body);
+    const json = await new PortfolioApi(accessToken).createPortfolio(req.body);
     return res.json(json.data);
   } catch(e) {
     return res.status(e.status || 400).end(e.message);
