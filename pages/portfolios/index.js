62c784825295dd290ec07aeda7a74cdcae51d3a5
lasha tatulashvili <lashatatu@gmail.com>
2021-02-01 18:29:49 +0400

    toast messages

--- pages/portfolios/index.js
+++ pages/portfolios/index.js
@@ -72,12 +72,13 @@ const Portfolios = ( { portfolios: initialPortfolios } ) => {
   );
 };
 
-export async function getStaticProps () {
+export async function getStaticProps() {
   const json = await new PortfolioApi().getAll();
   const portfolios = json.data;
   return {
-    props: { portfolios }
-  };
+    props: { portfolios },
+    revalidate: 1
+  }
 }
 
 export default Portfolios;
