0ec8d9734365c2a45eaf8975e4c7b95390d6c418
lasha tatulashvili <lashatatu@gmail.com>
2021-02-01 14:59:22 +0400

    api problem

--- actions/portfolios.js
+++ actions/portfolios.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
-import { useApiHandler } from './index';
+import { fetcher, useApiHandler } from '@/actions';
+import useSWR from 'swr';
 
-const createPortfolio=(data) => axios.post('/api/v1/portfolios', data);
+const createPortfolio = ( data ) => axios.post('/api/v1/portfolios', data);
 
+export const useCreatePortfolio = () => useApiHandler(createPortfolio);
 
-export const useCreatePortfolio=()=>useApiHandler(createPortfolio)
+export const useGetPortfolio = ( id ) => {
+  const { data, error, ...rest } = useSWR(id ? `/api/v1/portfolios/${id}` : null, fetcher);
+  return { data, error, loading: !data && !error, ...rest };
+};
