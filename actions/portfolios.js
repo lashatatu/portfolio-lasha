009b92c0b6b05ad5fe5c4b70268f150ef4f96aea
lasha tatulashvili <lashatatu@gmail.com>
2021-02-01 14:21:05 +0400

    api problem

--- actions/portfolios.js
+++ actions/portfolios.js
@@ -1,28 +1,7 @@
 import axios from 'axios';
-import { useState }from 'react'
+import { useApiHandler } from './index';
 
-function createPortfolio(data) {
-  return axios.post('/api/v1/portfolios', data);
-}
+const createPortfolio=(data) => axios.post('/api/v1/portfolios', data);
 
-export function useCreatePortfolio(){
-  const [reqState,setReqState]=useState({
-    error:null,
-    data:null,
-    loading:false
-  })
 
-  const createPortfolioHandler=async(...data)=>{
-    setReqState({error:null,data:null,loading: true});
-    try {
-    const json=await createPortfolio(...data)
-      setReqState({error: null,data: json.data,loading: false})
-    }
-    catch ( e ) {
-const message=(e.response && e.response.message) || 'oops, something went wrong...';
-      setReqState({error: message,data: null,loading: false})
-    }
-  }
-
-  return [createPortfolioHandler,{...reqState}]
-}
+export const useCreatePortfolio=()=>useApiHandler(createPortfolio)
