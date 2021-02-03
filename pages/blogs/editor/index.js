5f7571d755043d4973e1b60f0a14f479be1db783
lasha tatulashvili <lashatatu@gmail.com>
2021-02-03 23:22:04 +0400

    toast messages

--- pages/blogs/editor/index.js
+++ pages/blogs/editor/index.js
@@ -1,25 +1,26 @@
 import BaseLayout from '@/components/layouts/BaseLayout';
 import BasePage from '@/components/BasePage';
 import withAuth from '@/hoc/withAuth';
-import {Editor} from 'slate-simple-editor';
+import { Editor } from 'slate-simple-editor';
+import { useCreateBlog } from 'actions/blogs';
+import { toast } from 'react-toastify';
 
-const BlogEditor = ( { user, loading } ) => {
+const BlogEditor = ({user, loading}) => {
+  const [createBlog, {data: createdBlog, error}] = useCreateBlog();
 
-  const saveBlog=(data)=>{
-    console.log(data);
+  const saveBlog = async data => {
+    await createBlog(data)
+    alert('Blog was created!')
   }
-  return (
-     <BaseLayout
-        user={user}
-        loading={false}
-     >
-       <BasePage header={'Blog Editor'}>
-         <Editor onSave={(data)=>{
-           debugger
-         }}/>
-       </BasePage >
-     </BaseLayout >
-  );
-};
 
+  if (error) { toast.error(error.message); }
+
+  return (
+     <BaseLayout user={user} loading={loading}>
+       <BasePage>
+         <Editor onSave={saveBlog}/>
+       </BasePage>
+     </BaseLayout>
+  )
+}
 export default withAuth(BlogEditor)('admin');
