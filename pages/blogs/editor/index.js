b3fee0df393b5e9eea39f82a5ab4bb4ce021fe68
lasha tatulashvili <lashatatu@gmail.com>
2021-02-04 21:59:12 +0400

    toast messages

--- pages/blogs/editor/index.js
+++ pages/blogs/editor/index.js
@@ -4,13 +4,15 @@ import withAuth from '@/hoc/withAuth';
 import { Editor } from 'slate-simple-editor';
 import { useCreateBlog } from 'actions/blogs';
 import { toast } from 'react-toastify';
+import {useRouter} from 'next/router';
 
 const BlogEditor = ({user, loading}) => {
-  const [createBlog, {data: createdBlog, error}] = useCreateBlog();
+  const router=useRouter();
+  const [createBlog, {data: createdBlog, error,loading:blogLoading}] = useCreateBlog();
 
   const saveBlog = async data => {
-    await createBlog(data)
-    alert('Blog was created!')
+    const createdBlog= await createBlog(data)
+    router.push('/blogs/editor/[id]', `/blogs/editor/${createdBlog._id}`)
   }
 
   if (error) { toast.error(error.message); }
@@ -18,7 +20,7 @@ const BlogEditor = ({user, loading}) => {
   return (
      <BaseLayout user={user} loading={loading}>
        <BasePage>
-         <Editor onSave={saveBlog}/>
+         <Editor onSave={saveBlog} loading={blogLoading}/>
        </BasePage>
      </BaseLayout>
   )
