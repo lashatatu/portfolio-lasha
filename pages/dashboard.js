ca10a894fbdfe3e17f4faf38be06c9d4d48d2858
lasha tatulashvili <lashatatu@gmail.com>
2021-02-06 16:34:23 +0400

    toast messages

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -3,28 +3,63 @@ import BasePage from '@/components/BasePage';
 import auth0, { withAuth } from 'utils/auth0';
 import { Col, Row } from 'reactstrap';
 import Masthead from 'components/shared/Masthead';
+import PortDropdown from 'components/shared/Dropdown';
 import Link from 'next/link';
 import BlogApi from 'lib/api/blogs';
 
 const Dashboard = ({user, blogs}) => {
 
-  const renderBlogs=(blogs,status)=>(
-    <ul className={"user-blogs-list"}>
+  const createOption = ( blogStatus ) => {
+    return blogStatus === 'draft' ? { view: 'Publish story', value: 'published' }
+       : { view: 'make a draft', value: 'draft' };
+  };
+
+  const createOptions = ( blog ) => {
+
+    const option = createOption(blog.status);
+    return [
       {
-        blogs.filter(blog=>blog.status===status).map(blog=>
-        <li key={blog._id}>
-          <Link href={"/blogs/editor/[id]"} as={`/blogs/editor/${blog._id}`}>
-            <a>{blog.title}</a>
-          </Link>
-        </li>
-        )
+        key: `${blog._id}-published`, text: option.view, handlers: {
+          onClick: () => {
+            alert(`changing status to - ${option.value}`);
+          }
+        }
+      },
+      {
+        key: `${blog._id}-delete`, text: 'Delete', handlers: {
+          onClick: () => {
+            alert(`clicking delete! ${blog._id}`);
+          }
+        }
       }
-    </ul>
-  )
+    ];
+  };
+
+  const renderBlogs = ( blogs, status ) => (
+     <ul className={'user-blogs-list'}>
+       {
+         blogs.filter(blog => blog.status === status).map(blog =>
+            <li key={blog._id}>
+              <Link
+                 href={'/blogs/editor/[id]'}
+                 as={`/blogs/editor/${blog._id}`}
+              >
+                <a >{blog.title}</a >
+              </Link >
+              <PortDropdown items={createOptions(blog)}/>
+            </li >
+         )
+       }
+     </ul >
+  );
 
   return (
-     <BaseLayout navClass="transparent" user={user} loading={false}>
-       <Masthead imagePath="/images/home-bg.jpg" />
+     <BaseLayout
+        navClass="transparent"
+        user={user}
+        loading={false}
+     >
+       <Masthead imagePath="/images/home-bg.jpg"/>
        <BasePage className="blog-user-page">
          <Row>
            <Col md="6" className="mx-auto text-center">
