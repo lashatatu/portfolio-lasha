1cb8dd2f43d74d0174cc3dd5f4edcd4118eff358
lasha tatulashvili <lashatatu@gmail.com>
2021-02-06 15:57:10 +0400

    toast messages

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -3,9 +3,25 @@ import BasePage from '@/components/BasePage';
 import auth0, { withAuth } from 'utils/auth0';
 import { Col, Row } from 'reactstrap';
 import Masthead from 'components/shared/Masthead';
+import Link from 'next/link';
 import BlogApi from 'lib/api/blogs';
 
 const Dashboard = ({user, blogs}) => {
+
+  const renderBlogs=(blogs,status)=>(
+    <ul className={"user-blogs-list"}>
+      {
+        blogs.filter(blog=>blog.status===status).map(blog=>
+        <li key={blog._id}>
+          <Link href={"/blogs/editor/[id]"} as={`/blogs/editor/${blog._id}`}>
+            <a>{blog.title}</a>
+          </Link>
+        </li>
+        )
+      }
+    </ul>
+  )
+
   return (
      <BaseLayout navClass="transparent" user={user} loading={false}>
        <Masthead imagePath="/images/home-bg.jpg" />
@@ -13,9 +29,11 @@ const Dashboard = ({user, blogs}) => {
          <Row>
            <Col md="6" className="mx-auto text-center">
              <h2 className="blog-status-title"> Published Blogs </h2>
+             {renderBlogs(blogs,'published')}
            </Col>
            <Col md="6" className="mx-auto text-center">
              <h2 className="blog-status-title"> Draft Blogs </h2>
+             {renderBlogs(blogs,'draft')}
            </Col>
          </Row>
        </BasePage>
