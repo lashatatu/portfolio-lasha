cf5228971c4f2d4dc57b3ee61e727e5389786d07
lasha tatulashvili <lashatatu@gmail.com>
2021-02-13 20:24:01 +0400

    active link

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,35 @@
+import { useEffect, useRef, useState } from 'react';
 import BaseLayout from '@/components/layouts/BaseLayout';
+import BasePage from '@/components/BasePage';
 import { Col, Container, Row } from 'reactstrap';
 import Typed from 'react-typed';
-import { useGetUser } from 'actions/user';
-import { useEffect, useRef, useState } from 'react';
-import BasePage from '@/components/BasePage';
+import { useGetUser } from '@/actions/user';
 
-const ROLES = ['Developer', 'Tech Lover', 'Team Player', 'Coursor Creator', 'React.js'];
-
+const ROLES = ['Developer', 'Tech Lover', 'Team Player', 'Course Creator', 'React.js', 'Angular'];
 const Index = () => {
-
   const [isFlipping, setIsFlipping] = useState(false);
   const { data, loading } = useGetUser();
   const flipInterval = useRef();
-
   useEffect(() => {
-    startAnimation()
-    return ()=>flipInterval.current && clearInterval(flipInterval.current)
-  },[])
-
-  const startAnimation=()=>{
-    flipInterval.current=setInterval(()=>{
-      setIsFlipping(prevFlipping=>!prevFlipping)
-    },20000)
-  }
-
+    startAnimation();
+    return () => flipInterval.current && clearInterval(flipInterval.current);
+  }, []);
+  const startAnimation = () => {
+    flipInterval.current = setInterval(() => {
+      setIsFlipping(prevFlipping => !prevFlipping);
+    }, 20000);
+  };
   return (
      <BaseLayout
         user={data}
         loading={loading}
-        navClass={'transparent'}
+        navClass="transparent"
         className={`cover ${isFlipping ? 'cover-orange' : 'cover-blue'}`}
      >
-       <BasePage indexPage title={"portfolio - Lasha Tatulashvili"}>
+       <BasePage
+          indexPage
+          title="Portfolio - Filip Jerga"
+       >
          <div className="main-section">
            <div className="background-image">
              <img src="/images/background-index.png"/>
@@ -41,69 +39,63 @@ const Index = () => {
                <Col md="6">
                  <div className="hero-section">
                    <div className={`flipper ${isFlipping ? 'isFlipping' : ''}`}>
-                     <div className="image image-1">
-                       <div className="hero-section-content">
-                         <h2> Full Stack Web Developer </h2>
-                         <div className="hero-section-content-intro">
-                           Have a look at my portfolio and job history.
-                         </div>
-                     </div >
-                     <img
-                        className="image"
-                        src="/images/section-1.png"
-                     />
-                     <div className="shadow-custom">
-                       <div className="shadow-inner"> </div >
-                     </div >
-                   </div >
-                   <div className="back">
-                     <div className="image image-2">
-                       <div className="hero-section-content">
-                         <h2>React and Next is Amazing!</h2>
-                         <div className="hero-section-content-intro">
-                           Software developer ready for a project of any type!
-                         </div>
+                     <div className="front">
+                       <div className="image image-1">
+                         <div className="hero-section-content">
+                           <h2 > Full Stack Web Developer </h2 >
+                           <div className="hero-section-content-intro">
+                             Have a look at my portfolio and job history.
+                           </div >
+                         </div >
+                       </div >
+                       <div className="shadow-custom">
+                         <div className="shadow-inner"></div >
                        </div >
                      </div >
-                     <img
-                        className="image"
-                        src="/images/section-2.png"
-                     />
-                     <div className="shadow-custom-orange">
-                       <div className="shadow-inner"></div >
+                     <div className="back">
+                       <div className="image image-2">
+                         <div className="hero-section-content">
+                           <h2 >React and Next is Amazing!</h2 >
+                           <div className="hero-section-content-intro">
+                             Software developer ready for a project of any type!
+                           </div >
+                         </div >
+                       </div >
+
+                       <div className="shadow-custom shadow-custom-orange">
+                         <div className="shadow-inner"></div >
+                       </div >
                      </div >
                    </div >
                  </div >
-               </div >
-             </Col >
-             <Col
-                md="6"
-                className="hero-welcome-wrapper"
-             >
-               <div className="hero-welcome-text">
-                 <h1 >
-                   This is the Portfolio website of Lasha Tatulashvili.
-                   Get informed, collaborate and discover projects I was working on through the years!
-                 </h1 >
-               </div >
-
-               <Typed
-                  loop
-                  strings={ROLES}
-                  typeSpeed={70}
-                  backSpeed={70}
-                  backDelay={1000}
-                  loopCount={0}
-                  className={'self-typed'}
-                  cursorCar={'|'}
+               </Col >
+               <Col
+                  md="6"
+                  className="hero-welcome-wrapper"
                >
-               </Typed >
-               <div className="hero-welcome-bio">
-                 <h1 >
-                   Let's take a look on my work.
-                 </h1 >
-               </div >
-             </Col >
+                 <div className="hero-welcome-text">
+                   <h1 >
+                     Welcome to the portfolio website of Filip Jerga.
+                     Get informed, collaborate and discover projects I was working on through the years!
+                   </h1 >
+                 </div >
+                 <Typed
+                    loop
+                    typeSpeed={70}
+                    backSpeed={70}
+                    strings={ROLES}
+                    backDelay={1000}
+                    loopCount={0}
+                    showCursor
+                    className="self-typed"
+                    cursorChar="|"
+                 />
+                 <div className="hero-welcome-bio">
+                   <h1 >
+                     Let's take a look on my work.
+                   </h1 >
+                 </div >
+               </Col >
              </Row >
            </Container >
          </div >
@@ -111,5 +103,4 @@ const Index = () => {
      </BaseLayout >
   );
 }
-
 export default Index;
