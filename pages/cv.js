69dc6006aafd6007efd0a84d006fd457b886ecc3
lasha tatulashvili <lashatatu@gmail.com>
2021-02-14 07:39:55 +0400

    active link

--- pages/cv.js
+++ pages/cv.js
@@ -1,16 +1,17 @@
 import BaseLayout from '@/components/layouts/BaseLayout';
 import BasePage from '@/components/BasePage';
-import {useGetUser} from '@/actions/user';
+import { useGetUser } from '@/actions/user';
 import { Row, Col } from 'reactstrap';
 
 const Cv = () => {
-  const {data, loading}=useGetUser();
+  const { data, loading } = useGetUser();
   return (
      <BaseLayout user={data} loading={loading}>
-       <BasePage >
+       <BasePage
+          title="My Experiences - Lasha Tatulashvili">
          <Row>
            <Col md={{size: 8, offset: 2}}>
-             <iframe style={{width: '100%', height: '800px'}} src="/public/cvLasha.pdf"/>
+             <iframe style={{width: '100%', height: '800px'}} src="/cvLasha.pdf"/>
            </Col>
          </Row>
        </BasePage >
